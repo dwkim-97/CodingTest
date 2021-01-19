4deb1f87a2d4f3c7bcc2e26dd6f455215a031df7
DONGWOOK KIM <62870938+dwkim-97@users.noreply.github.com>
2021-01-19 10:29:21 +0900

    1/19
    1/19

---
+++ Programmers/Python/[코딩테스트연습]튜플.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = []
+    splitted = s[2:-2].split("},{")
+    splitted2 = sorted([spl.split(',') for spl in splitted], key=lambda x:len(x))
+
+    answer.append(int(splitted2[0][0]))
+    for i in range(1, len(splitted2)):
+        for s in splitted2[i]:
+            if s not in splitted2[i-1]:
+                answer.append(int(s))
+                break
+
+    print(splitted)
+    print(splitted2)
+    print(answer)
+    return answer
+
+solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")
