8a47e81555455447b0a3aeecc27a76690bc243a0
DONGWOOK KIM <62870938+dwkim-97@users.noreply.github.com>
2021-02-02 18:31:33 +0900

    2/2
    2/2

---
+++ Programmers/Python/[코딩테스트연습]입국심사.py
@@ -0,0 +1,19 @@
+def solution(n, times):
+    answer = max(times) * n
+    left = 1
+    right = answer
+    while left < right:
+        mid = (left + right) // 2
+        capability = 0
+        for t in times:
+            capability += mid // t
+        if capability >= n:
+            right = mid
+            if mid < answer:
+                answer = mid
+        else:
+            left = mid + 1
+
+    return answer
+
+print(solution(6, [7,10]))
