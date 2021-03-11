a8b7cf6322d37eb2a5f37bc470875157c6ad6b93
DONGWOOK KIM <62870938+dwkim-97@users.noreply.github.com>
2021-03-11 18:29:24 +0900

    3/11 [3차]자동완성
    3/11 [3차]자동완성

---
+++ Programmers/Java/Auto_Completion.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Auto_Completion {
+    public static void main(String[] args){
+//        System.out.println(solution(new String[]{"go", "gone", "guild"}));
+        System.out.println(solution(new String[]{"abc","def","ghi","jklm"}));
+        System.out.println(solution(new String[]{"word","war","warrior","world"}));
+    }
+
+    static Node root = new Node();
+
+    static int solution(String[] words) {
+        int answer = 0;
+
+        for(String word: words){
+            insert(word);
+        }
+
+        return search(root, 0);
+    }
+
+    static void insert(String word){
+        Node curNode = root;
+        for(int i = 0; i < word.length(); i++){
+            Node newRoot = curNode.put(word.charAt(i));
+            curNode = newRoot;
+        }
+        curNode.put('*');
+    }
+
+    static int search(Node root, int cnt){
+        if(root.childCount == 1){
+            return cnt;
+        }
+        int count = 0;
+        for(char key: root.childNodes.keySet()){
+            if(key == '*'){
+                count += cnt;
+            }
+            else{
+                count += search(root.childNodes.get(key), cnt+1);
+            }
+        }
+        return count;
+    }
+
+    static class Node{
+        private HashMap<Character, Node> childNodes = new HashMap<>();
+        private int childCount = 0;
+
+        Node put(char c){
+            childCount++;
+            if(!childNodes.containsKey(c)) {
+                childNodes.put(c, new Node());
+            }
+            return childNodes.get(c);
+        }
+    }
+}
