f26b37bc0aeccd1c8b8b0784aa192b0fe97d9173
DONGWOOK KIM <62870938+dwkim-97@users.noreply.github.com>
2021-04-04 17:08:19 +0900

    4/4 지형 이동
    4/4 지형 이동

---
+++ Programmers/Java/Terrain_Movement.java
@@ -0,0 +1,132 @@
+import java.util.*;
+
+public class Terrain_Movement {
+    public static void main(String[] argv){
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new int[][]{{1, 4, 8, 10}, {5, 5, 5, 5}, {10, 10, 10, 10}, {10, 10, 10, 20}}, 3));
+    }
+
+    static class Solution {
+        int[][] groups;
+        int[][] land;
+        int n, height;
+        ArrayList<Edge> edges;
+        public int solution(int[][] land, int height) {
+            this.n = land.length;
+            this.height = height;
+            this.groups = new int[n][n];
+            this.land = land;
+
+            int group_cnt = 0;
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(groups[i][j] == 0){
+                        group_cnt++;
+                        coloring(group_cnt, new int[]{i,j});
+                    }
+                }
+            }
+
+            this.edges = new ArrayList<>();
+
+            checking();
+
+            Collections.sort(edges);
+
+            return MST(group_cnt);
+        }
+
+        int MST(int group_cnt){
+            int[] roots = new int[group_cnt+1];
+            for(int i = 0; i < group_cnt+1; i++)
+                roots[i] = i;
+
+            int idx = 0, edge_cnt = 0, cost = 0;
+            while(edge_cnt < group_cnt-1){
+                Edge e = edges.get(idx);
+                if(roots[e.big_g] != roots[e.small_g]){
+                    int old_root = roots[e.small_g];
+                    for(int i = 0; i < roots.length; i++){
+                        if(roots[i] == old_root)
+                            roots[i] = roots[e.big_g];
+                    }
+                    cost += e.cost;
+                    edge_cnt++;
+                }
+                idx++;
+            }
+            return cost;
+        }
+
+        void checking(){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(i+1 < n && groups[i+1][j] != groups[i][j]){
+                        edges.add(new Edge(groups[i][j], groups[i+1][j], Math.abs(land[i][j] - land[i+1][j])));
+                    }
+                    if(i-1 >= 0 && groups[i-1][j] != groups[i][j]){
+                        edges.add(new Edge(groups[i][j], groups[i-1][j], Math.abs(land[i][j] - land[i-1][j])));
+                    }
+                    if(j+1 < n && groups[i][j+1] != groups[i][j]){
+                        edges.add(new Edge(groups[i][j], groups[i][j+1], Math.abs(land[i][j] - land[i][j+1])));
+                    }
+                    if(j-1 >= 0 && groups[i][j-1] != groups[i][j]){
+                        edges.add(new Edge(groups[i][j], groups[i][j-1], Math.abs(land[i][j] - land[i][j-1])));
+                    }
+                }
+            }
+        }
+
+        void coloring(int group_cnt, int[] pos){
+            Queue<int[]> queue = new LinkedList<>();
+
+            queue.add(pos);
+            this.groups[pos[0]][pos[1]] = group_cnt;
+            while(!queue.isEmpty()){
+                int[] popped = queue.poll();
+                int i = popped[0], j = popped[1];
+                if(i+1 < n && groups[i+1][j] == 0 && Math.abs(land[i+1][j] - land[i][j]) <= height){
+                    this.groups[i+1][j] = group_cnt;
+                    queue.add(new int[]{i+1,j});
+                }
+                if(i-1 >= 0 && groups[i-1][j] == 0 && Math.abs(land[i-1][j] - land[i][j]) <= height){
+                    this.groups[i-1][j] = group_cnt;
+                    queue.add(new int[]{i-1,j});
+                }
+                if(j+1 < n && groups[i][j+1] == 0 && Math.abs(land[i][j+1] - land[i][j]) <= height){
+                    this.groups[i][j+1] = group_cnt;
+                    queue.add(new int[]{i,j+1});
+                }
+                if(j-1 >= 0 && groups[i][j-1] == 0 && Math.abs(land[i][j-1] - land[i][j]) <= height){
+                    this.groups[i][j-1] = group_cnt;
+                    queue.add(new int[]{i,j-1});
+                }
+            }
+        }
+
+        class Edge implements Comparable<Edge>{
+            int big_g, small_g, cost;
+            public Edge(int g1, int g2, int cost){
+                if(g1 > g2){
+                    this.big_g = g1;
+                    this.small_g = g2;
+                }else{
+                    this.big_g = g2;
+                    this.small_g = g1;
+                }
+                this.cost = cost;
+            }
+
+            @Override
+            public int compareTo(Edge e){
+                return this.cost - e.cost;
+            }
+
+            public void show(){
+                System.out.println(big_g + ", "+ small_g+": "+cost);
+            }
+
+
+        }
+    }
+}
